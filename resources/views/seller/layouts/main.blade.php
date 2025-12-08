a497e44e4d85192ecc9903b4174e65aa65933ec2
abidbe <abid.i.ft20@mail.umy.ac.id>
2025-12-08 20:31:06 +0700

    fix theme

--- resources/views/seller/layouts/main.blade.php
+++ resources/views/seller/layouts/main.blade.php
@@ -6,6 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Laravel') }}</title>
+    <script>
+        (function() {
+            const savedTheme = localStorage.getItem('theme') || 'retro';
+            document.documentElement.setAttribute('data-theme', savedTheme);
+        })();
+    </script>
     <!-- Styles -->
     @vite(['resources/css/app.css', 'resources/css/custom.css'])
     @stack('styles')
