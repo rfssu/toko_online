61fdd947b6d0b33f27d677326abfeaa957399484
abidbe <abid.i.ft20@mail.umy.ac.id>
2025-12-18 18:41:34 +0700

    - add poto di asset

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,5 +2,6 @@ import "./vendor.js";
 import "./ajax.js";
 import Swal from "sweetalert2";
 import "../../node_modules/select2/dist/js/select2.full.min.js";
+import '../assets/photos/bakpia.jpg';
 
 globalThis.Swal = Swal;
